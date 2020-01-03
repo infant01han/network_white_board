090ecce93a1c482a08e5a360fc88dd6d08f4c096
infant01han <50282805+infant01han@users.noreply.github.com>
2020-01-03 16:59:21 +0800

    Delete Server.py

--- Server.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2020/1/3 16:10
-# @Author  : Han lei
-# @Email   : hanlei5012@163.com
-# @File    : Server.py.py
-# @Software: PyCharm
-print('world')
-print('hello')
